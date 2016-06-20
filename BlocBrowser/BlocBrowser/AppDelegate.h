460b39315068a0aefac993b3677ea268b6129f34
Ryan Rizzo <ryarriz@gmail.com>
2016-06-20 16:27:28 -0400

    Checkpoint 23 Complete

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Ryan Rizzo on 6/18/16.
+//  Copyright © 2016 Ryan Rizzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Ryan Rizzo on 6/18/16.
+//  Copyright © 2016 Ryan Rizzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
